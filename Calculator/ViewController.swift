14025aad781ca9ae9d598695b594d4662dd05e13
ssh <kathishivaram@gmail.com>
2019-05-16 10:40:05 +0530

    initial setup

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by apple on 5/16/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
